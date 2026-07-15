85a89492a3c043d679c3c1ef7d9a5a0c60292ce7
xerjester <reijikung02@hotmail.com>
2026-07-16 01:02:23 +0700

    Disable prepared statements in Go code

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -16,9 +17,19 @@ func InitDB() {
 
 	connString := "postgresql://postgres.asdaptdcvwmvzjnefiah:Jobchan273159@aws-0-ap-southeast-2.pooler.supabase.com:6543/postgres"
 
+	config, err := pgxpool.ParseConfig(connString)
+	if err != nil {
+		log.Printf("Unable to parse connection string: %v\n", err)
+		return
+	}
+
+	// Disable prepared statements for Transaction Pooler compatibility
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeExec
+	config.ConnConfig.StatementCacheCapacity = 0
+	config.ConnConfig.DescriptionCacheCapacity = 0
+
 	ctx := context.Background()
-	var err error
-	Pool, err = pgxpool.New(ctx, connString)
+	Pool, err = pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		log.Printf("Unable to create connection pool: %v\n", err)
 	}
